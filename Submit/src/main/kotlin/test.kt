ca78ed115e9a0d60e22b2606cd2bbb331398a579
phvmhieu <phamvanhieu06032003@gmail.com>
2023-12-05 15:17:42 +0700

    check

---
+++ Submit/src/main/kotlin/test.kt
@@ -0,0 +1,37 @@
+fun main(args: Array<String>) {
+//    var n: Int? = readLine()?.toInt()
+//    if(n != null) {
+//        while (n > 0) {
+//            var numCase: Int? = readLine()?.toInt()
+//            var array: IntArray = IntArray(numCase!!)
+//            for (i in array.indices) {
+//                var tmp: Int? = readLine()?.toInt()
+//                if (tmp != null) {
+//                    array[i] = tmp
+//                }
+//            }
+//            var tmp = array[0]
+//            var size = array.size
+//            for (i in 0 until size) {
+//                if (tmp != array[i]) {
+//                    if(i < size - 1) {
+//                        if (tmp != array[i + 1]) {
+//                            println("1")
+//                            break
+//                        } else {
+//                            println("${i + 1}")
+//                            break
+//                        }
+//                    }else{
+//                        println("${i + 1}")
+//                    }
+//                }
+//            }
+//            n--
+//        }
+//    }
+    var testCase: Int? = readLine()?.toInt()
+    while (testCase!! > 0){
+        var arraySize: Int? = readLine()?.toInt()
+    }
+}
